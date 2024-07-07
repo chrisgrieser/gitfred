186be15782ffaac94ab182b1699297b8fa45bb8e
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-07-07 12:29:27 +0200

    improv: appearance and sorting of issue search

--- scripts/my-github-issues.js
+++ scripts/my-github-issues.js
@@ -17,27 +17,35 @@ function alfredMatcher(str) {
 function run() {
 	const resultsNumber = 50; // api allows up to 100
 	const username = $.getenv("github_username");
-	const apiURL = `https://api.github.com/search/issues?q=involves:${username}&per_page=${resultsNumber}`;
+
+	// DOCS https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#list-issues-assigned-to-the-authenticated-user--parameters
+	const apiURL = `https://api.github.com/search/issues?q=involves:${username}&sort=updated&per_page=${resultsNumber}`;
 
 	const issues = JSON.parse(app.doShellScript(`curl -sL "${apiURL}"`)).items.map(
 		(/** @type {GithubIssue} */ item) => {
 			const issueAuthor = item.user.login;
-			const authoredByMe = issueAuthor === username;
-
 			const isPR = Boolean(item.pull_request);
 			const merged = Boolean(item.pull_request?.merged_at);
 			const title = item.title;
 			const repo = (item.repository_url.match(/[^/]+$/) || "")[0];
 			const comments = item.comments > 0 ? "💬 " + item.comments.toString() : "";
+			const open = item.state === "open";
+			const closed = item.state === "closed";
+			const reason = item.state_reason;
+			const labels = item.labels.map((label) => `[${label.name}]`).join(" ");
 
-			let icon = authoredByMe ? "🚩 " : "";
-			if (item.state === "open" && isPR) icon += "🟩 ";
-			else if (item.state === "closed" && isPR && merged) icon += "🟪 ";
-			else if (item.state === "closed" && isPR && !merged) icon += "🟥 ";
-			else if (item.state === "closed" && !isPR) icon += "🟣 ";
-			else if (item.state === "open" && !isPR) icon += "🟢 ";
-			if (title.toLowerCase().includes("request") || title.includes("FR")) icon += "🙏 ";
-			if (title.toLowerCase().includes("bug")) icon += "🪲 ";
+			const subtitle = [`#${item.number}`, repo, comments.toString(), labels]
+				.filter(Boolean)
+				.join("   ");
+
+			// icon
+			let icon = issueAuthor === username ? "✏️ " : "";
+			if (open && isPR) icon += "🟩 ";
+			else if (closed && isPR && merged) icon += "🟪 ";
+			else if (closed && isPR && !merged) icon += "🟥 ";
+			else if (open && !isPR) icon += "🟢 ";
+			else if (closed && reason === "not_planned") icon += "⚪ ";
+			else if (closed && reason === "completed") icon += "🟣 ";
 
 			let matcher = alfredMatcher(item.title) + " " + alfredMatcher(repo) + " " + item.state;
 			if (isPR) matcher += " pr";
@@ -45,7 +53,7 @@ function run() {
 
 			return {
 				title: icon + title,
-				subtitle: `#${item.number}  ${repo}   ${comments}`,
+				subtitle: subtitle,
 				match: matcher,
 				arg: item.html_url,
 				quicklookurl: item.html_url,
