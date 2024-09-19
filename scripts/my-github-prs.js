f0f0e051a4f11e369f42f7f67e725b371e6b56de
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-09-19 13:01:26 +0200

    fix: better error message when no response from GitHub API

--- scripts/my-github-prs.js
+++ scripts/my-github-prs.js
@@ -47,14 +47,27 @@ function humanRelativeDate(isoDateStr) {
 	return str.replace(/m(?= ago$)/, "min"); // "m" -> "min" (more distinguishable from "month")
 }
 
+/** @param {string} url @return {string} */
+function httpRequest(url) {
+	const queryURL = $.NSURL.URLWithString(url);
+	const data = $.NSData.dataWithContentsOfURL(queryURL);
+	return $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding).js;
+}
+
 //──────────────────────────────────────────────────────────────────────────────
 
 // biome-ignore lint/correctness/noUnusedVariables: alfred_run
 function run() {
 	const username = $.getenv("github_username");
 	const apiURL = `https://api.github.com/search/issues?q=author:${username}+is:pr+is:open&per_page=100`;
+	const response = JSON.parse(httpRequest(apiURL));
+	if (!response) {
+		return JSON.stringify({
+			items: [{ title: "No response from GitHub.", subtitle: "Try again later.", valid: false }],
+		});
+	}
 
-	const openPrs = JSON.parse(app.doShellScript(`curl -sL "${apiURL}"`)).items.map(
+	const openPrs = JSON.parse(response).items.map(
 		(/** @type {GithubIssue} */ item) => {
 			const repo = (item.repository_url.match(/[^/]+$/) || "")[0];
 			const comments = item.comments > 0 ? "💬 " + item.comments.toString() : "";
