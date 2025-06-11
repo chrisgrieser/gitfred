f05ad51cf5f4de48333c0aca244b93d18848d514
Adam Brown <acourtneybrown@gmail.com>
2025-06-11 15:07:03 -0700

    Revert "Check `op` for GitHub token only if configured"
    This reverts commit 82786c794e5450e4255c6055c0ce996250743ac5.

--- scripts/op-github-token.sh
+++ scripts/op-github-token.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
 # shellcheck disable=2154,SC1071
 
-if [[ "$github_token_from_op_plugin" == "true" ]]; then
-    echo $(op plugin run -- gh auth status --show-token | grep 'Token: ' | sed 's/.*: \(.*\)/\1/')
-fi
+echo $(op plugin run -- gh auth status --show-token | grep 'Token: ' | sed 's/.*: \(.*\)/\1/')
